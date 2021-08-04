9c89dcc1b5f09bcc24bbd967b5ab7c8e2b787771
Jake <jakedves@gmail.com>
2021-08-04 07:29:42 +0100

    Added base card

--- Glassmorphism/ContentView.swift
+++ Glassmorphism/ContentView.swift
@@ -7,13 +7,19 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Glassmorphism: View {
     @State private var loadImagePressed = false
     
     var body: some View {
-        VStack {
-            Text("Hi")
-            button
+        ZStack {
+            Image("")
+            
+            VStack {
+                Spacer()
+                card
+                Spacer()
+                button
+            }
         }
     }
     
@@ -21,11 +27,48 @@ struct ContentView: View {
         Button("Load Image") {
             loadImagePressed = true
         }
+        .padding(.vertical)
+        .padding(.horizontal, 140)
+        .background(Color(UIColor.quaternaryLabel))
+        .cornerRadius(12)
+    }
+    
+    // An exmaple card
+    private var card: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.red)
+                .cornerRadius(20)
+            
+            cardContent
+        }
+        .frame(width: 350, height: 550, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+    
+    private var cardContent: some View {
+        VStack {
+            HStack {
+                Text("Example Card")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(30)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            Text("This card is used to demonstrate the glassmorphism effect.")
+                .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Glassmorphism()
     }
 }
